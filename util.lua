d5fa90e1cec533f2cfda4cf095b96921bd3c7fb5
MansfieldTX-MattR <matt.reid@mansfieldtexas.gov>
2026-03-02 14:31:54 -0600

    Add conversion to string for `StatusType` enum

--- util.lua
+++ util.lua
@@ -8,6 +8,19 @@ StatusType = {
   Initializing = 5,
 }
 
+---@alias StatusTypeName "OK" | "Compromised" | "Fault" | "NotPresent" | "Missing" | "Initializing"
+
+---@param status StatusType
+---@return StatusTypeName
+function StatusTypeToString(status)
+  for key, value in pairs(StatusType) do
+    if value == status then
+      return key
+    end
+  end
+  error("Invalid status type: "..tostring(status))
+end
+
 
 function DebugPrint(msg)
   print("[DEBUG] "..msg)
