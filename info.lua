568035b6257c9335c703f092ec719eb93b0a9756
MansfieldTX-MattR <matt.reid@mansfieldtexas.gov>
2026-04-16 10:01:42 -0500

    Update version to 1.0.3

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
     Name = "Blackmagic Videohub",
     PrettyName = "Blackmagic Videohub",
-    Version = "1.0.2",
+    Version = "1.0.3",
     BuildVersion = "0.0.0.0",
     Id = "9103262e-7dc3-4f5b-a092-495c05d5d260",
     Author = "Matt Reid",
